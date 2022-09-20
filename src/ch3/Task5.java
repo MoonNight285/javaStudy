8b234de1b9511e61512bce413b8d19759ce51855
MoonNight285 <kimjyjh11@naver.com>
2022-09-20 16:39:24 +0900

    chapter_03 보너스 문제1(미완성 - 제목만 작성함)

---
+++ src/ch3/Task5.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class Task5 {
+    public static void main(String[] args) {
+        // Bouns1 - 129p
+        // 다음은 3 * 4의 2차원 배열을 만들고 이곳에 0 ~ 9 범위의 정수를 랜덤하게
+        // 저장한 후 2차원 배열과 합을 출력하는 프로그램의 실행 결과와 코드이다.
+        // 빈 곳에 적절한 코드를 삽입하라.
+    }
+}
