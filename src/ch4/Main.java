ed18da2640244901e835cd8e57a7619f4a0c0ee0
MoonNight285 <kimjyjh11@naver.com>
2022-09-22 16:05:39 +0900

    chapter_04 클래스 오버로딩 사용법 예시 작성

--- src/ch4/Main.java
+++ src/ch4/Main.java
@@ -33,5 +33,47 @@ public class Main {
         System.out.println(car2.type);
         System.out.println(car2.gas);
         System.out.println(car2.size);
+
+        Car4 toress = new Car4("토레스", "SUV");
+        toress.info();
+
+        Car4 santefe = new Car4("싼타페", "SUV");
+        santefe.info();
+
+        Car4 canival = new Car4();
+        canival.name = "카니발";
+        canival.type = "SUV";
+        canival.info();
+
+        Car5 grander1 = new Car5();
+        grander1.getInfo();
+
+        Car5 k7 = new Car5("K7", "세단", "준대형");
+        k7.getInfo();
+        k7.getInfo("전기");
+        k7.getInfo("가솔린", 4000);
+        k7.getInfo("가솔린", "4000");
+        k7.getInfo(4000, "경유");
+
+        sum(10, 20);
+        sum(3.14, 5.28);
+    }
+
+    public static void sum(int a, int b) {
+        int result = a + b;
+        System.out.println("두 수의 덧셈은 " + result + "입니다.");
+    }
+
+    public static void sum(double a, double b) {
+        double result = a + b;
+        System.out.println("두 수의 덧셈은 " + result + "입니다.");
+    }
+
+    public static void sum(int a, double b) {
+
+    }
+
+    public static void sum(double a, int b) {
+
     }
 }
