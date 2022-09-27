6f41724887ea6f01b89e944a8233b892023c303b
MoonNight285 <kimjyjh11@naver.com>
2022-09-27 10:38:36 +0900

    chapter_05 상속 예제 작성

---
+++ src/ch5/Main.java
@@ -0,0 +1,17 @@
+package ch5;
+
+public class Main {
+    public static void main(String[] args) {
+        // 2022-09-27
+        System.out.println("\n ----- 상속 -----\n");
+
+        Point p = new Point();
+        p.set(1, 2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3, 4);
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
